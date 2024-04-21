e5fb4dc6a2f55f1fc1bae5a137e48f2b767df5fa
magzh <magzhi94@gmail.com>
2024-04-21 13:53:58 +0500

    Add files via upload

---
+++ bot/private.py
@@ -0,0 +1,8 @@
+from aiogram.types import BotCommand
+
+
+private=[
+    BotCommand(command='start',description='start'),
+    BotCommand(command='make_a_tour',description='create a tour'),
+    BotCommand(command='understand_new_information', description='speak with bot')
+]
